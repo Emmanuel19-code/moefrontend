9dfce3dea111dbf9eb4ff003ee554e7de6724616
unknown <emmanueladane@gmail.com>
2025-07-23 15:12:34 +0000

    update

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,12 +1,18 @@
 import * as React from "react"
 
-import type {
-  ToastActionElement,
-  ToastProps,
-} from "@/components/ui/toast"
+type ToastActionElement = React.ReactElement;
+
+type ToastProps = {
+  title?: React.ReactNode;
+  description?: React.ReactNode;
+  action?: ToastActionElement;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+};
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
+
 
 type ToasterToast = ToastProps & {
   id: string
